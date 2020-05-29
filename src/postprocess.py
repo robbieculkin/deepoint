d850451984ff80304454e8f8713f06a39be55609
Robbie Culkin <robbie.culkin@intusurg.com>
2020-05-28 22:31:03 -0700

    nms implemented

--- src/postprocess.py
+++ src/postprocess.py
@@ -5,6 +5,8 @@ def non_mamima_suppression(m, window=10, thresh=0.2):
     window: size of tiles to limit maximum
     thresh: min value of point to be considered
     '''
+    #3D->2D
+    m = m.reshape(m.shape[1::-1])
     new_m = np.zeros(m.shape)
     #iterate over window x window sized tiles
     for ii in range(0,m.shape[0],window):
@@ -14,4 +16,5 @@ def non_mamima_suppression(m, window=10, thresh=0.2):
             w_max_idx = np.unravel_index(w.argmax(), w.shape)
             if w[w_max_idx] > thresh:
                 new_m[w_max_idx[0]+ii, w_max_idx[1]+jj] = 1
-    return new_m
+    #2D->3D
+    return new_m.reshape((*m.shape[::-1],1))
